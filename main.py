e95dd03ca26e91edb337f385c085d5740af74b64
Muhammad Tahmid <muhammadzulqifletahmid@gmail.com>
2020-09-09 20:56:42 -0400

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import random
+
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='.')
@@ -14,10 +15,19 @@ async def ping(ctx):
     await ctx.send(f'{round(client.latency * 1000)}ms')
 
 @client.command()
-async  def drop(ctx, *, questions):
+async  def drop(ctx):
     responses = ['Dusty Depot',
                  'Flush Factory',
                  'Lama Depot']
-    await ctx.send(f'Question: {questions}\nAnswer: {random.choice(responses)}')
+    await ctx.author.send((random.choice(responses)))
 
-client.run('')
+@client.command()
+async def start(ctx):
+    await ctx.send('Ok, started check your DMs and continue from there!')
+    await ctx.author.send ('Ok, We will start from here. Please do not write .start again, it '
+                           'might mess your account up with this bot. Start with .help to list '
+                           'all of the available commands. ')
+
+
+
+client.run('NzUyMzc1NTU0Mzg0MzMwODQ0.X1WuVg.9pInn1pL_nleICImaWuWiJ9mTUo')
